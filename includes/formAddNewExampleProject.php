9e00f47923a3e05bc472d9b1a0b2fdd02cb775a1
SimonB87 <buryans@gmail.com>
2020-06-10 22:37:59 +0200

    add new project - preparation

--- includes/formAddNewExampleProject.php
+++ includes/formAddNewExampleProject.php
@@ -5,10 +5,10 @@
 <br>
 <form method="post" action="processNewExampleProject.php">
 
-<label for="username">Username :</label><br>
+<label for="username"><strong>Username :</strong></label><br>
 <input type="text" id="username" name="username"><br><br>
 
-<label for="username">Password :</label><br>
+<label for="username"><strong>Password :</strong></label><br>
 <input type="password" name="password"><br><br>
 
 <input type="submit" value="Submit">
@@ -17,6 +17,8 @@
 //Plánovaný formulář:<br>
 . (-) Username ... autofilled <br>
 . (-) UserEmail ... autofilled<br>
+. (-) čas podání žádosti ... autofilled<br>
+. (-) jméno typového řešení ke kterému se projekt váže - comboBox - standardně to typové řešení, které je nyní zobrazneno je vybráno <br>
 . Jméno vzorového projektu - povinné!<br>
 . Lokalita projektu - povinné!<br>
 . Popis vzorového projektu - povinné!<br>
@@ -24,7 +26,15 @@
 . (+) Právní souvislosti<br>
 . Další zdroje1 - web? - povinné!<br>
 . (+) Další Zdroje2 - cokoliv?<br>
-- řádek v databázi - status (podáno, schváleno / zamítnuto)<br>
+- (-) status autofilled na (podáno) (jinak schváleno / zamítnuto / vráceno k přepracování)<br>
+
+//TODO:
+. Po podání nová obrazovka - místo echo include, kde bude menu a jen infromace pro usera o stavu podání (Děkujeme, či něco se pokazilo).
+
+//TODO:
+. založit sekci management podaných řešení
+. pro administrátora i pro uživatele
+. tabulka s projekty a jejich překlik na detail
 <br>
 <br>
 <hr>
