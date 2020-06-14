b8fff81eeaf17820f05ed0a74b15bd8bfbf39ec2
SimonB87 <buryans@gmail.com>
2020-06-14 23:16:45 +0200

    Edit of form saving process - all inputs stored

--- processNewExampleProject.php
+++ processNewExampleProject.php
@@ -1,18 +1,24 @@
 <?php
-
 $prefilled_userName = filter_input(INPUT_POST, 'prefilled_userName');
-
-$prefilled_firstName = filter_input(INPUT_POST, 'prefilled_firstName');
-
-$prefilled_lastName = filter_input(INPUT_POST, 'prefilled_lastName');
-
 $prefilled_email = filter_input(INPUT_POST, 'prefilled_email');
-
+$prefilled_firstName = filter_input(INPUT_POST, 'prefilled_firstName');
+$prefilled_lastName = filter_input(INPUT_POST, 'prefilled_lastName');
 $prefilled_submissionStatus = filter_input(INPUT_POST, 'prefilled_submissionStatus');
-
 $prefilled_submissionDate = filter_input(INPUT_POST, 'prefilled_submissionDate');
 
-if (!empty($prefilled_userName )) or (!empty($prefilled_email )){
+$projectGroup = filter_input(INPUT_POST, 'projectGroup');
+$projectName = filter_input(INPUT_POST, 'projectName');
+$projectLocality = filter_input(INPUT_POST, 'projectLocality');
+
+$projectReferenceMain = filter_input(INPUT_POST, 'projectReferenceMain');
+$projectReferenceOther = filter_input(INPUT_POST, 'projectReferenceOther');
+$projectDescription = filter_input(INPUT_POST, 'projectDescription');
+$projectCosts = filter_input(INPUT_POST, 'projectCosts');
+$projectLegalIssues = filter_input(INPUT_POST, 'projectLegalIssues');
+$administratorDecisionLetter = filter_input(INPUT_POST, 'administratorDecisionLetter');
+$submitterDecisionResponse = filter_input(INPUT_POST, 'submitterDecisionResponse');
+ 
+if (!empty($prefilled_userName )) {
     $host = "localhost";
     $dbusername = "root";
     $dbpassword = "";
@@ -23,17 +29,24 @@ if (!empty($prefilled_userName )) or (!empty($prefilled_email )){
       die('Connect Error ('. mysqli_connect_errno() .') ' . mysqli_connect_error());
     } else {
       
-      $sql = "INSERT INTO exampleprojects (prefilled_userName, prefilled_email) VALUES ('$prefilled_userName', '$prefilled_email')";
+      $sql = "INSERT INTO exampleprojects (prefilled_userName, prefilled_email, prefilled_firstName, prefilled_lastName, prefilled_submissionStatus, prefilled_submissionDate, projectGroup, projectName, projectLocality, projectReferenceMain, projectReferenceOther, projectDescription, projectCosts, projectLegalIssues, administratorDecisionLetter, submitterDecisionResponse) VALUES ('$prefilled_userName', '$prefilled_email', '$prefilled_firstName', '$prefilled_lastName', '$prefilled_submissionStatus', '$prefilled_submissionDate', '$projectGroup', '$projectName', '$projectLocality', '$projectReferenceMain', '$projectReferenceOther', '$projectDescription', '$projectCosts', '$projectLegalIssues', '$administratorDecisionLetter', '$submitterDecisionResponse')";
       
       if ($dbConnection->query($sql)){
-        echo "Nový záznam zapsán do databáze. Děkujeme.";
+        
+        include("includes/header.php");
+        echo "<h2>Nový záznam zapsán do databáze. Děkujeme</h2>.";
+        echo "</div>";
       } else {
-        echo "Chyba: ". $sql ." ". $dbConnection->error;
+
+        include("includes/header.php");
+        echo "<h2>Chyba: </h2><br>". $sql ." ". $dbConnection->error;
+        echo "</div>";
+
       }
         $dbConnection->close();
       }
   } else{
-    echo "Pole uživatelského jména nebo emailu by nemělo být prázdné";
+    echo "Pole uživatelského jména by nemělo být prázdné";
     die();
   }
 ?>
