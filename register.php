52d857a5c3f30ae24409020d2bdb858a3bc6f81e
SimonB87 <buryans@gmail.com>
2020-09-05 21:22:14 +0200

    Social network - edit of registration page

--- register.php
+++ register.php
@@ -10,6 +10,7 @@ require 'includes/form_handlers/login_handler.php';
 	<title>Cirkulární projekty | Web pro projekty v Cirkulární ekonomice a Oběhovém hospodářství</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
 	<link rel="stylesheet" type="text/css" href="assets/css/register.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/register.js"></script>
 
@@ -34,111 +35,117 @@ require 'includes/form_handlers/login_handler.php';
 
 
 	?>
+	<div id="wrapper"> 
 
-	<div class="thewrapped">
 
-		<div style="margin: 1rem auto;">
-  			<button class="button" style="background-color: #057705; border: none; color: white; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 0 auto; cursor: pointer;"><a href="http://obcevkruhu.cz" style="color:#fff;">Zpět na hlavní stránku</a></button>
-		</div>
+		<div class="thewrapped">
 
-		<div class="login_box">
-
-			<div class="login_header">
-				<h1>Cirkulární projekty</h1>
-				Přihlaste se nebo se registrujte níže!
-			</div>
-			<br>
-			<div id="first">
-
-				<form action="register.php" method="POST">
-					<input id="log_email" type="email" name="log_email" placeholder="Email" value="<?php
-					if(isset($_SESSION['log_email'])) {
-						echo $_SESSION['log_email'];
-					}
-					?>" required>
-					<br>
-					<input id="password" type="password" name="log_password" placeholder="Heslo">
-					<br>
-					<?php if(in_array("Email nebo heslo nejsou správné<br>", $error_array)) echo  "Email nebo heslo nejsou správné<br>"; ?>
-					<input id="signInButton" type="submit" name="login_button" value="Přihlásit se">
-					<br>
-					<a href="#" id="signup" class="signup">Nemáte účet? Registrovat lze zde!</a>
-
-				</form>
-
+			<div style="margin: 1rem auto;">
+					<button class="button" style="background-color: #e5b896; border: none; padding: 15px 32px; text-align: center; text-decoration: none; display: inline-block; font-size: 16px; margin: 0 auto; cursor: pointer;"><a href="http://obcevkruhu.cz" style="color:#000;">Zpět na hlavní stránku</a></button>
 			</div>
 
-			<div id="second">
+			<div class="login_box">
 
-				<form action="register.php" method="POST">
-					<input type="text" name="reg_fname" placeholder="First Name" value="<?php
-					if(isset($_SESSION['reg_fname'])) {
-						echo $_SESSION['reg_fname'];
-					}
-					?>" required>
-					<br>
-					<?php if(in_array("Your first name must be between 2 and 25 characters<br>", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
+				<div class="login_header">
+					<h1>Cirkulární projekty</h1>
+					Přihlaste se nebo se registrujte níže!
+				</div>
+				<br>
+				<div id="first">
 
+					<form action="register.php" method="POST">
+						<input id="log_email" type="email" name="log_email" placeholder="Email" value="<?php
+						if(isset($_SESSION['log_email'])) {
+							echo $_SESSION['log_email'];
+						}
+						?>" required>
+						<br>
+						<input id="password" type="password" name="log_password" placeholder="Heslo">
+						<br>
+						<?php if(in_array("Email nebo heslo nejsou správné<br>", $error_array)) echo  "Email nebo heslo nejsou správné<br>"; ?>
+						<input id="signInButton" type="submit" name="login_button" value="Přihlásit se">
+						<br>
+						<a href="#" id="signup" class="signup">Nemáte účet? Registrovat lze zde!</a>
 
+					</form>
 
+				</div>
 
-					<input type="text" name="reg_lname" placeholder="Last Name" value="<?php
-					if(isset($_SESSION['reg_lname'])) {
-						echo $_SESSION['reg_lname'];
-					}
-					?>" required>
-					<br>
-					<?php if(in_array("Your last name must be between 2 and 25 characters<br>", $error_array)) echo "Your last name must be between 2 and 25 characters<br>"; ?>
+				<div id="second">
 
-					<input type="email" name="reg_email" placeholder="Email" value="<?php
-					if(isset($_SESSION['reg_email'])) {
-						echo $_SESSION['reg_email'];
-					}
-					?>" required>
-					<br>
-
-					<input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php
-					if(isset($_SESSION['reg_email2'])) {
-						echo $_SESSION['reg_email2'];
-					}
-					?>" required>
-					<br>
-					<?php if(in_array("Email already in use<br>", $error_array)) echo "Email already in use<br>";
-					else if(in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>";
-					else if(in_array("Emails don't match<br>", $error_array)) echo "Emails don't match<br>"; ?>
-
-
-					<input type="password" name="reg_password" placeholder="Password" required>
-					<br>
-					<input type="password" name="reg_password2" placeholder="Confirm Password" required>
-					<br>
-					<?php if(in_array("Your passwords do not match<br>", $error_array)) echo "Your passwords do not match<br>";
-					else if(in_array("Your password can only contain english characters or numbers<br>", $error_array)) echo "Your password can only contain english characters or numbers<br>";
-					else if(in_array("Your password must be betwen 5 and 30 characters<br>", $error_array)) echo "Your password must be betwen 5 and 30 characters and include one or more characters.<br>"; ?>
+					<form action="register.php" method="POST">
+						<input type="text" name="reg_fname" placeholder="First Name" value="<?php
+						if(isset($_SESSION['reg_fname'])) {
+							echo $_SESSION['reg_fname'];
+						}
+						?>" required>
+						<br>
+						<?php if(in_array("Your first name must be between 2 and 25 characters<br>", $error_array)) echo "Your first name must be between 2 and 25 characters<br>"; ?>
 
 
-					<input type="submit" name="register_button" value="Register">
-					<br>
 
-					<?php if(in_array("<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>", $error_array)) echo "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>"; ?>
-					<a href="#" id="signin" class="signin">Already have an account? Sign in here!</a>
-				</form>
+
+						<input type="text" name="reg_lname" placeholder="Last Name" value="<?php
+						if(isset($_SESSION['reg_lname'])) {
+							echo $_SESSION['reg_lname'];
+						}
+						?>" required>
+						<br>
+						<?php if(in_array("Your last name must be between 2 and 25 characters<br>", $error_array)) echo "Your last name must be between 2 and 25 characters<br>"; ?>
+
+						<input type="email" name="reg_email" placeholder="Email" value="<?php
+						if(isset($_SESSION['reg_email'])) {
+							echo $_SESSION['reg_email'];
+						}
+						?>" required>
+						<br>
+
+						<input type="email" name="reg_email2" placeholder="Confirm Email" value="<?php
+						if(isset($_SESSION['reg_email2'])) {
+							echo $_SESSION['reg_email2'];
+						}
+						?>" required>
+						<br>
+						<?php if(in_array("Email already in use<br>", $error_array)) echo "Email already in use<br>";
+						else if(in_array("Invalid email format<br>", $error_array)) echo "Invalid email format<br>";
+						else if(in_array("Emails don't match<br>", $error_array)) echo "Emails don't match<br>"; ?>
+
+
+						<input type="password" name="reg_password" placeholder="Password" required>
+						<br>
+						<input type="password" name="reg_password2" placeholder="Confirm Password" required>
+						<br>
+						<?php if(in_array("Your passwords do not match<br>", $error_array)) echo "Your passwords do not match<br>";
+						else if(in_array("Your password can only contain english characters or numbers<br>", $error_array)) echo "Your password can only contain english characters or numbers<br>";
+						else if(in_array("Your password must be betwen 5 and 30 characters<br>", $error_array)) echo "Your password must be betwen 5 and 30 characters and include one or more characters.<br>"; ?>
+
+
+						<input type="submit" name="register_button" value="Register">
+						<br>
+
+						<?php if(in_array("<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>", $error_array)) echo "<span style='color: #14C800;'>You're all set! Go ahead and login!</span><br>"; ?>
+						<a href="#" id="signin" class="signup">Již máte účet? Vstupte zde!</a>
+					</form>
+				</div>
+
 			</div>
 
-		</div>
-
-		<div class="demoUser">
-    		<div class="login_header" style="background:coral;">
-        		<h1>Demo přihlášení</h1>
-    		</div>
-    		<div>
-				<p><strong>Email:</strong> <span id="userDemoEmail">demo@uzivatel.cz</span></p>
-				<p><strong>Heslo:</strong> <span id="userDemoEmail">Demo123456</span></p>
-				<p> <button type="button" class="btn btn-success" onclick="userDemoAccount();"> Použít demo účet </button> </p>
+			<div class="demoUser">
+					<div class="login_header">
+							<h1>Demo přihlášení</h1>
+					</div>
+					<div>
+					<p class="text-center"><strong>Email:</strong> <span id="userDemoEmail">demo@uzivatel.cz</span></p>
+					<p class="text-center"><strong>Heslo:</strong> <span id="userDemoEmail">Demo123456</span></p>
+					<p class="text-center"> <button type="button" class="btn btn-reload" onclick="userDemoAccount();"> Použít demo účet </button> </p>
+				</div>
 			</div>
+
 		</div>
 
 	</div>
+
+	<div id="bg"> </div>
 
 	<script>
 	function userDemoAccount() {
