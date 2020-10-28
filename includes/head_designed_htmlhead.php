e69b838d6e65c2d7c37c986fae1d31a81b1560a5
SimonB87 <buryans@gmail.com>
2020-10-28 15:30:21 +0100

    removed generic title in header-html

--- includes/head_designed_htmlhead.php
+++ includes/head_designed_htmlhead.php
@@ -35,8 +35,6 @@ $userLoggedInNameFillerPlaceholder = $user['first_name'] . " " . $user['last_nam
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>Vítejte na webu Circularních projektů</title>
-
   <!-- for carousel -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 
