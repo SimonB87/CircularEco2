14076e975e24b6563bab4f2ce63878e919f54d74
SimonB87 <buryans@gmail.com>
2021-01-08 02:18:27 +0100

    reset passwprd debug

--- resetPassword.php
+++ resetPassword.php
@@ -25,10 +25,16 @@ require 'includes/form_handlers/login_handler.php';
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="assets/js/register.js"></script>
 
+  <style>
+  .wrapper {
+    overflow-y: scroll;
+  }
+  </style>
+
 </head>
 <body>
 
-	<div id="wrapper"> 
+	<div id="wrapper" class="wrapper"> 
 
 		<div class="thewrapped">
 
@@ -46,7 +52,7 @@ require 'includes/form_handlers/login_handler.php';
 				<br>
 				<div id="first">
 
-					<form action="resetPassword.php" method="POST">
+					<form method="POST">
 						<input id="password1" type="password" name="new_password_1" placeholder="Nové heslo" autocomplete="off" required>
 						<br>
             <input id="password2" type="password" name="new_password_2" placeholder="Opakujte opět heslo" autocomplete="off" required>
@@ -62,50 +68,38 @@ require 'includes/form_handlers/login_handler.php';
 
             $code = $_GET["code"];
 
-            $getEmailQuery = mysqli_query($con_password, "SELECT email FROM resetPassword WHERE code='$code'");
+            $getEmailQuery = mysqli_query($con_password, "SELECT email, code FROM resetPassword WHERE code='$code'");
             if(mysqli_num_rows($getEmailQuery) == 0 ){
               $password_message =  "Stránka s požadavkem nebyla nalezena.";
             }
 
             $row = mysqli_fetch_array($getEmailQuery);
             $userEmail = $row['email'];
+            $reqCode = $row['code'];
 
-            //test
-            echo "test row - userEmail: " . $userEmail . "<br>";
-            //test
 
             if(isset($_POST['new_password_1'])) {
 
               $new_password_1 = strip_tags($_POST['new_password_1']);
               $new_password_2 = strip_tags($_POST['new_password_2']);
 
-                            //test
-                            echo "test - new_password_1:: $new_password_1 :: <br>";
-                            echo "test - new_password_2:: $new_password_2 :: <br>";
-                            //test
-
               $new_password_md5 = md5($new_password_1);
 
               //$password_query = mysqli_query($con, "SELECT password FROM users WHERE email='$userEmail'");
 
               if($new_password_1 === $new_password_2) {
-
-              //test
-              echo "passwords match OK <br>";
-              //test
             
-              $update_password_query = mysqli_query($con, "UPDATE users SET password='$new_password_md5' WHERE email='$userEmail'");
-              $update_query = mysqli_query($con, "UPDATE users SET friend_array='test1' WHERE email='$userEmail'");
+              $update_password_query = mysqli_query($con_password, "UPDATE users SET password='$new_password_md5' WHERE email='$userEmail'");
               
               if($update_password_query) {
-                $delete_request = mysqli_query($con, "DELETE FROM resetPassword WHERE code='$code'");
-                $password_message = "Heslo bylo úspěšně změněno.<br><br>";
+                $delete_request = mysqli_query($con_password, "DELETE FROM resetPassword WHERE code='$code'");
+                $password_message = "Heslo bylo úspěšně změněno.<br>";
               } else {
-                $password_message = "Heslo nemohlo být změněno.<br><br>";
+                $password_message = "Heslo nemohlo být změněno.<br>";
               }
 
               } else {
-                $password_message = "Obě zadaná hesla se musí schodovat!<br><br>";
+                $password_message = "Obě zadaná hesla se musí schodovat!<br>";
               }
 
             }
